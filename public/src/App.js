7661853dea632ab5cced6ba77bc3407234216e42
Hitesh Prajapat <142203204+Hitesh464156@users.noreply.github.com>
2024-08-24 18:50:51 +0530

    Add files via upload

---
+++ public/src/App.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hitesh Prajapat</h1>
+                <p>Contact No.: 7424810080</p>
+                <p>Email: 2022ucp1655@mnit.ac.in</p>
+                <a href="https://www.linkedin.com/in/hitesh-prajapat-170647277/" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a>
+            </header>
+            <section>
+                <h2>Education</h2>
+                <ul>
+                    <li>10th: Adarsh Vidya Mandir, Khairthal || %=92.33(RBSE) (2019)</li>
+                    <li>12th: Engineer's Point Senior Secondary School, Khairthal || %=98.40(RBSE) (2021)</li>
+                    <li>B-tech: Malaviya National Institute Of Technology, Jaipur || CGPA=7.02 (Computer Science & Engineering) (Jul 2022- June 2024)</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Coding Skills</h2>
+                <ul>
+                    <li>Python, C, C++, OOPS, DSA, Dynamic Programming, Greedy Approach</li>
+                    <li>Database Management System (DBMS::SQL)</li>
+                    <li>Full Stack Web Developer (HTML, CSS, JS, Express.js, Node.js)</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Hobbies</h2>
+                <ul>
+                    <li>Multitasking</li>
+                    <li>Playing Cricket</li>
+                    <li>Watching Movies (Bollywood, South)</li>
+                    <li>Love To Handle Difficult Situations</li>
+                </ul>
+            </section>
+            <section>
+                <h2>Projects</h2>
+                <ul>
+                    <li>Covid-19 Tracker</li>
+                    <li>Portfolio</li>
+                    <li>Movie Recommendation System</li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+export default App;
